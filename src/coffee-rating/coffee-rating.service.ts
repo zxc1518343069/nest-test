c3fb6dc1e83debf0e93d18bafaa90a2c2a40b0e7
achen <1518343069@qq.com>
2022-08-21 15:59:31 +0800

    update: 有关Injectable 的scope 问题

--- src/coffee-rating/coffee-rating.service.ts
+++ src/coffee-rating/coffee-rating.service.ts
@@ -1,6 +1,8 @@
 import { CoffeesService } from './../coffees/coffees.service';
-import { Injectable } from '@nestjs/common';
+import { Injectable, Scope } from '@nestjs/common';
 
+// Scope.TRANSIENT 瞬态 多实例 默认单实例 即每次注册是都是一个全新的实例
+// Scope.REQUEST 发生请求时才实例化
 @Injectable()
 export class CoffeeRatingService {
   // 这时候这个模块就可以用了
